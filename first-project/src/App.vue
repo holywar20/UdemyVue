fe376336f89eb1cf80f194f6c056256eac58f928
Bryan Winter <bwinter@AM.IDTCORP.NET>
2021-09-13 17:23:09 -0400

    Started using components and CLI based projects.

---
+++ first-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+	<ul>
+		<li v-for="friend in friends" :key="friend.id">{{ friend.name }}</li>
+	</ul>
+</div>	
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			friends:[
+				{
+					id : "Bryan",
+					name : "Bryan Winter",
+					phone: "123.453.1231",
+					email: "test@test.com"
+				},{
+					id : "Other",
+					name : "John Carmack",
+					phone: "123.453.1231",
+					email: "test@test.com"
+			}
+		]}
+	}
+}
+</script>
